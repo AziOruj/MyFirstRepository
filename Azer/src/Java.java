16883157583249b43cdf7d7f9b154358edd9c72e
orujov <orujovazar1994@gmail.com>
2021-06-09 23:13:12 +0400

    a

--- Azer/src/Java.java
+++ Azer/src/Java.java
@@ -1,21 +1,7 @@
 import java.sql.SQLOutput;
 
 public class Java {
-    public static void main(String[] args) {
-        System.out.print("Cem: = ");
-        System.out.println("Hello feature/Dev1");
-    }
-    public void topla(int a,int b){
-        System.out.print("Cem: ");
-        System.out.println(a+b);
-
-    }
-    public void  cixma(int a,int b){
-        System.out.println("Cem: = "+(a+b));
-    }
-    public void cixam(int a,int b){
-        System.out.println("Cem: = "+(a-b));
-    }
+   
 }
 
 
