6227dae7e124a5d553b3c69abab929a0b90c40cf
orujov <orujovazar1994@gmail.com>
2021-06-09 14:31:45 +0400

    topla methodu yaradildi master

--- Azer/src/Java.java
+++ Azer/src/Java.java
@@ -2,4 +2,7 @@ public class Java {
     public static void main(String[] args) {
         System.out.println("Hello");
     }
+    public void topla(int a , int b){
+        System.out.println(a+b);
+    }
 }
