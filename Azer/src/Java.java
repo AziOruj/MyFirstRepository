97b3077b48738c70339e0981d38a159d7aee4ee4
orujov <orujovazar1994@gmail.com>
2021-06-09 14:49:28 +0400

    Cem yaradildi Feature/Dev1

--- Azer/src/Java.java
+++ Azer/src/Java.java
@@ -1,6 +1,6 @@
 public class Java {
     public static void main(String[] args) {
-
+        System.out.print("Cem: = ");
         System.out.println("Hello feature/Dev1");
     }
     public void topla(int a,int b){
